dcd1421b94159bfd5b13ae4b3bf8433995b9b909
Nick Schot <nickschot@gmail.com>
2017-04-01 16:07:05 +0200

    Implement semantic-ui

--- client/ember-cli-build.js
+++ client/ember-cli-build.js
@@ -8,14 +8,6 @@ module.exports = function(defaults) {
       enabled: EmberApp.env() !== 'production',
       extensions: ['js']
     },
-    sassOptions: {
-      includePaths: [
-        'bower_components/bootstrap/scss'
-      ],
-      options: {
-        precision: 8
-      }
-    },
     'ember-cli-babel': {
       includePolyfill: true
     }
@@ -33,12 +25,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
-  const bootstrapImportPath = 'bower_components/bootstrap/js/dist/';
-  app.import(bootstrapImportPath + 'util.js');
-  app.import(bootstrapImportPath + 'button.js');
-  app.import(bootstrapImportPath + 'collapse.js');
-  app.import(bootstrapImportPath + 'dropdown.js');
 
   return app.toTree();
 };
